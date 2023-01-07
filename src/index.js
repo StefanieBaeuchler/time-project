f5f59de2caaf05b2f75a7813623eec12d5c136af
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2023-01-07 18:17:54 +0100

    Updated search section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function updateTime() {
   let viennaElement = document.querySelector("#vienna");
+  if (viennaElement){
   let viennaDateElement = viennaElement.querySelector(".date");
   let viennaTimeElement = viennaElement.querySelector(".time");
   let viennaTime = moment().tz("Europe/Vienna");
@@ -7,9 +8,10 @@ function updateTime() {
   viennaDateElement.innerHTML = viennaTime.format("MMMM Do YYYY");
   viennaTimeElement.innerHTML = `${viennaTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  )}`;
+  )}`;}
 
   let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement){
   let sydneyDateElement = sydneyElement.querySelector(".date");
   let sydneyTimeElement = sydneyElement.querySelector(".time");
   let sydneyTime = moment().tz("Australia/Sydney");
@@ -17,14 +19,14 @@ function updateTime() {
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
   sydneyTimeElement.innerHTML = `${sydneyTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  )}`;
+  )}`;}
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector(".cities");
+  let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
     <div>
