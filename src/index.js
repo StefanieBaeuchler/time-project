5dec002e6c9adf4783fdd6f73b621b9e0e30fa98
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2023-01-07 16:37:32 +0100

    Integrated moment.js and moment timezone.js library

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  let viennaElement = document.querySelector("#vienna");
+  let viennaDateElement = viennaElement.querySelector(".date");
+  let viennaTimeElement = viennaElement.querySelector(".time");
+  let viennaTime = moment().tz("Europe/Vienna");
+
+  viennaDateElement.innerHTML = viennaTime.format("MMMM Do YYYY");
+  viennaTimeElement.innerHTML = `${viennaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = `${sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
