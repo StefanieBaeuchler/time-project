992a20ad735571ff592f2aaca447863cf0cafdeb
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2023-01-07 18:29:00 +0100

    Added current location

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 function updateTime() {
   let viennaElement = document.querySelector("#vienna");
-  if (viennaElement){
-  let viennaDateElement = viennaElement.querySelector(".date");
-  let viennaTimeElement = viennaElement.querySelector(".time");
-  let viennaTime = moment().tz("Europe/Vienna");
+  if (viennaElement) {
+    let viennaDateElement = viennaElement.querySelector(".date");
+    let viennaTimeElement = viennaElement.querySelector(".time");
+    let viennaTime = moment().tz("Europe/Vienna");
 
-  viennaDateElement.innerHTML = viennaTime.format("MMMM Do YYYY");
-  viennaTimeElement.innerHTML = `${viennaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;}
+    viennaDateElement.innerHTML = viennaTime.format("MMMM Do YYYY");
+    viennaTimeElement.innerHTML = `${viennaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   let sydneyElement = document.querySelector("#sydney");
-  if (sydneyElement){
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
 
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = `${sydneyTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;}
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+    sydneyTimeElement.innerHTML = `${sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
